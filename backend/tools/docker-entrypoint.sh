7b7112114bdeeea922df6f3657f0a568e5659db3
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-05-05 03:51:26 +0900

    :dizzy:[update]backend superuser作成

--- backend/tools/docker-entrypoint.sh
+++ backend/tools/docker-entrypoint.sh
@@ -13,6 +13,6 @@ python manage.py makemigrations
 python manage.py migrate
 
 # superuserの作成
-#echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_SU_USER', '$DJANGO_SU_MAIL', '$DJANGO_SU_PASSWORD') if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists() else None" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_SU_USER', '$DJANGO_SU_MAIL', '$DJANGO_SU_PASSWORD') if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists() else None" | python manage.py shell
 
 exec "$@"
