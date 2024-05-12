0ff06f44dd0e6eedce359d3f5f85f195949625ed
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-05-12 13:07:26 +0900

    :dizzy:[update]token取得処理まとめ

--- frontend/static_vol/js/modules/auth.js
+++ frontend/static_vol/js/modules/auth.js
@@ -1,55 +1,36 @@
 "use strict";
 
-const getAccessToken = () => {
-    try {
-        const accessToken = localStorage.getItem('accessToken');
-        if (accessToken === null) {
-            return null;//未ログイン
-        }
-        if (!accessToken) {//todo:test (undefinedなど)
-            throw new Error('accessToken is invalid');
-        }
-        return accessToken;
-    } catch (error) {
-        // ここではエラーをキャッチせず呼び出し側でキャッチする
-        return error;
+//localstrageにtokenがkey自体ない=>ログアウト状態
+//tokenがundefined=>何かがおかしい
+const getToken = (nameToken) => {
+    const token = localStorage.getItem(nameToken);
+    if (token === null) {
+        return null;//未ログイン
     }
+    if (!token) {//todo:test (undefinedなど)
+        throw new Error(`${nameToken} is invalid`);
+    }
+    return token;
 }
 
-const getRefreshToken = () => {
-    try {
-        const refreshToken = localStorage.getItem('refreshToken');
-        if (!refreshToken) {
-            throw new Error('refreshToken is invalid');
-        }
-        return refreshToken;
-    } catch (error) {
-        return error;
-    }
-}
+const refreshToken = async () => {
+    const refreshToken = getToken('refreshToken');
 
-const refreshRefreshToken = async () => {
-    try {
-        const refreshToken = getRefreshToken();
-
-        // SimpleJWTのリフレッシュトークン発行はbodyにrefreshを渡す仕様
-        const response = await fetch('http://localhost:8001/token/refresh/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ 'refresh': `${refreshToken}` })
-        });
-        if (response.ok) {
-            const refreshData = await response.json();
-            localStorage.setItem('accessToken', refreshData.access);
-            return true;
-        }
-        // refreshToken無効。
-        return false;
-    } catch (error) {
-        console.error('refreshRefreshToken:', error);
+    // SimpleJWTのリフレッシュトークン発行はbodyにrefreshを渡す仕様
+    const response = await fetch('http://localhost:8001/token/refresh/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ 'refresh': `${refreshToken}` })
+    });
+    if (response.ok) {
+        const refreshData = await response.json();
+        localStorage.setItem('accessToken', refreshData.access);
+        return true;
     }
+    // refreshToken無効。
+    return false;
 }
 
 const handleLogout = (ev) => {
@@ -96,11 +77,11 @@ const handleLogout = (ev) => {
 }
 
 const getIsLogin = async () => {
-    if ((getAccessToken()) === null) {//localstrageにaccessTokenがkey自体ない=>ログアウト状態
+    if ((getToken('accessToken')) === null) {//localstrageにaccessTokenがkey自体ない=>ログアウト状態
         return null;
     }
     const userData = await checkLoginStatus();
-    if (userData == null) {
+    if (userData === null) {
         return null;
     }
     if (userData.is_authenticated) {
@@ -112,7 +93,7 @@ const getIsLogin = async () => {
 
 const checkLoginStatus = async () => {
     try {
-        const accessToken = getAccessToken();
+        const accessToken = getToken('accessToken');
         const response = await fetch('http://localhost:8001/api/players/userinfo/', {
             method: 'GET',
             headers: {
@@ -123,10 +104,10 @@ const checkLoginStatus = async () => {
         if (response.ok) {
             return await response.json();
         } else if (response.status === 401) {
-            if (!await refreshRefreshToken()) {
+            if (!await refreshToken()) {
                 throw new Error('fail refresh token');
             }
-            const accessToken2 = getAccessToken();
+            const accessToken2 = getToken('accessToken');
 
             const response2 = await fetch('http://localhost:8001/api/players/userinfo/', {
                 method: 'GET',
@@ -178,4 +159,4 @@ const switchDisplayAccount = async () => {
     }
 }
 
-export { getAccessToken, refreshRefreshToken, switchDisplayAccount };
+export { getToken, refreshToken, switchDisplayAccount };
