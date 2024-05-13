4ffb27db25177eb85a0143af2dd8a6597fa79495
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-05-13 19:10:54 +0900

    :dizzy:[update]/api/players/未実装につき一旦OFF

--- frontend/static_vol/js/components/GameMatch.js
+++ frontend/static_vol/js/components/GameMatch.js
@@ -9,41 +9,41 @@ export default class extends PageBase {
         this.labelButton = '対戦を申し込む'; // TODO json
     }
 
-    async getListPlayers() {
-        return fetch('http://localhost:8001/api/players/')
-            .then(response => response.json())
-            .then(data => {
-                return data;
-            })
-            .catch(error => {
-                console.error('Error fetching users:', error);
-                throw error;
-            });
-    }
+    // async getListPlayers() {
+    //     return fetch('http://localhost:8001/api/players/')
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             return data;
+    //         })
+    //         .catch(error => {
+    //             console.error('Error fetching users:', error);
+    //             throw error;
+    //         });
+    // }
 
-    async renderHtml() {
-        const listUsers = await this.getListPlayers();
-        if (!listUsers || listUsers.length === 0) {
-            return '<div>No players found.</div>';
-        }
-        return `
-            <div class="listPlayers">
-                ${listUsers.map(player => `
-                <section class="unitListItem">
-                    <header class="unitListItem_header">
-                        <div class="unitPlayerName">
-                            <h3>${player.playername}</h3>
-                            <p class="thumb"><img src="//ui-avatars.com/api/?name=Aa Bb&background=e3ad03&color=ffffff" alt="" width="60" height="60"></p>
-                        </div>
-                        <button class="unitButton">${this.labelButton}</button>
-                    </header>
-                    <div class="unitListItem_content unitPlayerScore">
-                        <p>RANK: 4 (20勝3敗)</p>
-                        <p>過去の対戦成績: 2勝1敗</p>
-                    </div>
-                </section>
-                `).join('')}
-            </div>
-        `; // TODO json; 翻訳しやすくしたい
-    }
+    // async renderHtml() {
+    //     const listUsers = await this.getListPlayers();
+    //     if (!listUsers || listUsers.length === 0) {
+    //         return '<div>No players found.</div>';
+    //     }
+    //     return `
+    //         <div class="listPlayers">
+    //             ${listUsers.map(player => `
+    //             <section class="unitListItem">
+    //                 <header class="unitListItem_header">
+    //                     <div class="unitPlayerName">
+    //                         <h3>${player.playername}</h3>
+    //                         <p class="thumb"><img src="//ui-avatars.com/api/?name=Aa Bb&background=e3ad03&color=ffffff" alt="" width="60" height="60"></p>
+    //                     </div>
+    //                     <button class="unitButton">${this.labelButton}</button>
+    //                 </header>
+    //                 <div class="unitListItem_content unitPlayerScore">
+    //                     <p>RANK: 4 (20勝3敗)</p>
+    //                     <p>過去の対戦成績: 2勝1敗</p>
+    //                 </div>
+    //             </section>
+    //             `).join('')}
+    //         </div>
+    //     `; // TODO json; 翻訳しやすくしたい
+    // }
 }
