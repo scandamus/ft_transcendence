6efbe74936ddec902a4abc62f9cf18e55d32430c
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-05-14 20:48:36 +0900

    :dizzy:[update]player.model 表示名設定など

--- backend/scandamus/players/models.py
+++ backend/scandamus/players/models.py
@@ -8,44 +8,52 @@ from django.dispatch import receiver
 class PlayerProfile(models.Model):
     player = models.ForeignKey(
         User,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        verbose_name="プレイヤー"
     )
     avatar = models.ImageField(
         upload_to='uploads/avatar/',
         validators=[FileExtensionValidator(['jpg', 'png'])],
-        null=True
+        null=True,
+        verbose_name="アバター"
     )
     level = models.FloatField(
         validators=[MinValueValidator(0.0)],
-        default=0.0
+        default=0.0,
+        verbose_name="レベル"
     )
     play_count = models.IntegerField(
-        default=0
+        default=0,
+        verbose_name="総試合数"
     )
     win_count = models.IntegerField(
-        default=0
+        default=0,
+        verbose_name="勝った試合数"
     )
     id_42 = models.CharField(
         max_length=20,
-        verbose_name="42 Intra ID",
-        null=True
+        null=True,
+        verbose_name="42 Intra ID"
     )
     link_42 = models.CharField(
         max_length=255,
-        verbose_name="42 Intra link",
-        null=True
+        null=True,
+        verbose_name="42 Intra link"
     )
     lang = models.CharField(
         max_length=10,
         choices=[
             ('en', '英語'),
             ('ja', '日本語'),
-            ('after', '対戦後')
+            ('fr', 'フランス語'),
+            ('emoji', '絵文字')
         ],
-        default='en'
+        default='en',
+        verbose_name="言語設定"
     )
     data_created = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
+        verbose_name="登録日時"
     )
 
     def __str__(self):
