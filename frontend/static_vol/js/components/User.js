6d3b395f7a5fceaf5918d0297f9ae8c6a1e71b19
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-05-16 00:09:47 +0900

    :dizzy:[update]user レイアウト調整

--- frontend/static_vol/js/components/User.js
+++ frontend/static_vol/js/components/User.js
@@ -7,6 +7,7 @@ export default class extends PageBase {
     constructor(params) {
         super(params);
         this.userName = 'user1';
+        this.labelMatch = '対戦する';
         this.setTitle(`USER: ${this.userName}`);
         //afterRenderにmethod追加
         this.addAfterRenderHandler(this.showUserList.bind(this));
@@ -21,16 +22,19 @@ export default class extends PageBase {
                 </div>
                 <div class="blockPlayerDetail_detail">
                     <section class="blockFriends">
-                        <h3>Friends</h3>
-                        <div class="listFriends">
+                        <h3 class="blockFriends_title unitTitle1">Friends</h3>
+                        <div class="blockFriends_friends listFriends listLineDivide">
                         </div>
                     </section>
-                    <ul class="blockPlayerDetail_log logTournament">
-                        <li class="logTournament_item"><strong>RANK: 4</strong> <span>(2024/4/2 tournament52)</span></li>
-                        <li class="logTournament_item"><strong>RANK: 6</strong> <span>(2024/4/1 tournament45)</span></li>
-                        <li class="logTournament_item"><strong>RANK: 4</strong> <span>(2024/3/24 tournament42)</span></li>
-                        <li class="logTournament_item"><strong>RANK: 1</strong> <span>(2024/3/10 tournament40)</span></li>
-                    </ul>
+                    <section class="blockMatchLog">
+                        <h3 class="blockMatchLog_title unitTitle1">Match Log</h3>
+                        <ul class="blockMatchLog_log listMatchLog listLineDivide">
+                            <li class="listMatchLog_item"><strong>RANK: 4</strong> <span>(2024/4/2 tournament52)</span></li>
+                            <li class="listMatchLog_item"><strong>RANK: 6</strong> <span>(2024/4/1 tournament45)</span></li>
+                            <li class="listMatchLog_item"><strong>RANK: 4</strong> <span>(2024/3/24 tournament42)</span></li>
+                            <li class="listMatchLog_item"><strong>RANK: 1</strong> <span>(2024/3/10 tournament40)</span></li>
+                        </ul>
+                    </section>
                 </div>
             </div>
         `;
@@ -47,7 +51,9 @@ export default class extends PageBase {
                             <h4 class="unitFriend_name">${user.username}</h4>
                             <p class="unitFriend_thumb"><img src="//ui-avatars.com/api/?name=${user.username}&background=3cbbc9&color=ffffff" alt="" width="100" height="100"></p>
                         </header>
-                        <p class="unitFriend_button unitFriend_button-match"><button>対戦</button></p>
+                        <p class="unitFriendButton unitFriend_button-match">
+                            <button type="submit" id="btnLogoutForm" class="unitFriendButton_button unitButton">${this.labelMatch}</button>
+                        </p>
                     </section>
                   `);
                 listFriendsWrapper.innerHTML = userElements.join('');
