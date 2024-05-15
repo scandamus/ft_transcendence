e481c9da70ac24ed603aff5304074435a58ec36e
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-05-15 14:44:26 +0900

    :dizzy:[update]userList userページへ表示

--- frontend/static_vol/js/components/User.js
+++ frontend/static_vol/js/components/User.js
@@ -1,12 +1,15 @@
 'use strict';
 
 import PageBase from './PageBase.js';
+import { getUserList } from '../modules/users.js';
 
 export default class extends PageBase {
     constructor(params) {
         super(params);
         this.userName = 'user1';
         this.setTitle(`USER: ${this.userName}`);
+        //afterRenderにmethod追加
+        this.addAfterRenderHandler(this.showUserList.bind(this));
     }
 
     async renderHtml() {
@@ -16,24 +19,41 @@ export default class extends PageBase {
                     <p class="blockPlayerDetail_thumb thumb"><img src="//ui-avatars.com/api/?name=Gg Hh&background=872bac&color=ffffff" alt="" width="200" height="200"></p>
                     <p class="blockPlayerDetail_score unitBox">RANK: 4 <br>(70勝20敗)</p>
                 </div>
-                <ul class="blockPlayerDetail_log logTournament">
-                    <li class="logTournament_item"><strong>RANK: 4</strong> <span>(2024/4/2 tournament52)</span></li>
-                    <li class="logTournament_item"><strong>RANK: 6</strong> <span>(2024/4/1 tournament45)</span></li>
-                    <li class="logTournament_item"><strong>RANK: 4</strong> <span>(2024/3/24 tournament42)</span></li>
-                    <li class="logTournament_item"><strong>RANK: 1</strong> <span>(2024/3/10 tournament40)</span></li>
-                    <li class="logTournament_item"><strong>RANK: 3</strong> <span>(2024/2/8 tournament36)</span></li>
-                    <li class="logTournament_item"><strong>RANK: 4</strong> <span>(2024/2/8 tournament30)</span></li>
-                    <li class="logTournament_item"><strong>RANK: 4</strong> <span>(2024/1/26 tournament25)</span></li>
-                    <li class="logTournament_item"><strong>RANK: 4</strong> <span>(2024/1/24 tournament22)</span></li>
-                    <li class="logTournament_item"><strong>RANK: 4</strong> <span>(2024/1/20 tournament18)</span></li>
-                    <li class="logTournament_item"><strong>RANK: 10</strong> <span>(2024/1/16 tournament12)</span></li>
-                    <li class="logTournament_item"><strong>RANK: 4</strong> <span>(2024/1/12 tournament7)</span></li>
-                    <li class="logTournament_item"><strong>RANK: 1</strong> <span>(2024/1/11 tournament5)</span></li>
-                    <li class="logTournament_item"><strong>RANK: 4</strong> <span>(2024/1/10 tournament3)</span></li>
-                    <li class="logTournament_item"><strong>RANK: 2</strong> <span>(2024/1/5 tournament2)</span></li>
-                    <li class="logTournament_item"><strong>RANK: 4</strong> <span>(2024/1/1 tournament1)</span></li>
-                </ul>
+                <div class="blockPlayerDetail_detail">
+                    <section class="blockFriends">
+                        <h3>Friends</h3>
+                        <div class="listFriends">
+                        </div>
+                    </section>
+                    <ul class="blockPlayerDetail_log logTournament">
+                        <li class="logTournament_item"><strong>RANK: 4</strong> <span>(2024/4/2 tournament52)</span></li>
+                        <li class="logTournament_item"><strong>RANK: 6</strong> <span>(2024/4/1 tournament45)</span></li>
+                        <li class="logTournament_item"><strong>RANK: 4</strong> <span>(2024/3/24 tournament42)</span></li>
+                        <li class="logTournament_item"><strong>RANK: 1</strong> <span>(2024/3/10 tournament40)</span></li>
+                    </ul>
+                </div>
             </div>
         `;
+    }
+
+    showUserList() {
+        const listFriendsWrapper = document.querySelector('.listFriends');
+        getUserList()
+            .then(data => {
+                listFriendsWrapper.innerHTML = '';
+                const userElements = data.map(user => `
+                    <section class="unitFriend">
+                        <header class="unitFriend_header">
+                            <h4 class="unitFriend_name">${user.username}</h4>
+                            <p class="unitFriend_thumb"><img src="//ui-avatars.com/api/?name=${user.username}&background=3cbbc9&color=ffffff" alt="" width="100" height="100"></p>
+                        </header>
+                        <p class="unitFriend_button unitFriend_button-match"><button>対戦</button></p>
+                    </section>
+                  `);
+                listFriendsWrapper.innerHTML = userElements.join('');
+            })
+            .catch(error => {
+                console.error('getUserInfo failed:', error);
+            })
     }
 }
