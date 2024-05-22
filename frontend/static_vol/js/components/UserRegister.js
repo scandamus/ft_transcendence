34964cb4c68a21720efe4a77cea34868eb2bbacc
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-05-22 20:14:24 +0900

    :sparkles:[add]ユーザー登録validate (front)

--- frontend/static_vol/js/components/UserRegister.js
+++ frontend/static_vol/js/components/UserRegister.js
@@ -7,25 +7,89 @@ export default class extends PageBase {
         super(params);
         this.setTitle('UserRegister');
         this.labelButton = '確認する'; // TODO json
+        //afterRenderにmethod追加
+        this.addAfterRenderHandler(this.watchValidateForm.bind(this));
+        this.addAfterRenderHandler(this.listenRegister.bind(this));
     }
 
     async renderHtml() {
         return `
             <form class="formUserRegister blockForm" action="" method="post">
                 <dl class="unitFormInput">
-                    <dt class="unitFormInput_label"><label for="">name</label></dt>
-                    <dd class="unitFormInput_input"><input type="text" /></dd>
+                    <dt class="unitFormInput_label"><label for="registUsername">name</label></dt>
+                    <dd class="unitFormInput_input"><input type="text" id="registUsername" placeholder="Enter username" pattern="^(?!.*?[-_][-_])[a-zA-Z0-9](?:[-_]?[a-zA-Z0-9])[a-zA-Z0-9]$" minlength="3" maxlength="30" required /></dd>
                 </dl>
                 <dl class="unitFormInput">
-                    <dt class="unitFormInput_label"><label for="">email</label></dt>
-                    <dd class="unitFormInput_input"><input type="text" /></dd>
+                    <dt class="unitFormInput_label"><label for="registPassword">password</label></dt>
+                    <dd class="unitFormInput_input"><input type="password" id="registPassword" placeholder="Enter password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$" required /></dd>
                 </dl>
                 <dl class="unitFormInput">
-                    <dt class="unitFormInput_label"><label for="">password</label></dt>
-                    <dd class="unitFormInput_input"><input type="password" /></dd>
+                    <dt class="unitFormInput_label"><label for="registPasswordConfirm">password(confirm)</label></dt>
+                    <dd class="unitFormInput_input"><input type="password" id="registPasswordConfirm" placeholder="Enter password(confirm)" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$" required /></dd>
                 </dl>
-                <button type="submit" class="formUserRegister_button unitButton">${this.labelButton}</button>
+                <button type="submit" id="btnRegisterForm" class="formUserRegister_button unitButton">${this.labelButton}</button>
             </form>
         `;
+    }
+
+    watchValidateForm() {
+        //focus外れたらvalidチェック
+        //passは両方入力履歴あれば一致チェック
+    }
+
+    listenRegister() {
+        const btnRegister = document.getElementById('btnRegisterForm');
+        btnRegister.addEventListener('click', this.handleRegister.bind(this));
+        this.addListenEvent(btnRegister, this.handleRegister, 'click');
+    }
+
+    handleRegister(ev) {
+        ev.preventDefault();
+        const elUsername = document.getElementById('registUsername');
+        const elPassword = document.getElementById('registPassword');
+        const elPasswordConfirm = document.getElementById('registPasswordConfirm');
+        let numError = 0;
+        if (elUsername.validity.patternMismatch) {
+            console.error('username is invalid.');
+            numError++;
+        }
+        if (elPassword.validity.patternMismatch) {
+            console.error('password is invalid.');
+            numError++;
+        }
+        if (elPassword.value !== elPasswordConfirm.value) {
+            console.error('The passwords you entered are not the same.');
+            numError++;
+        }
+        if (numError) {
+            return ;
+        }
+
+        const data = {
+            username: elUsername.value,
+            password: elPassword.value
+        };
+
+        fetch('http://localhost:8001/api/players/validate/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('register failed with status: ' + response.status);
+                }
+                return response.json();
+            })
+            .then(() => {
+                sessionStorage.setItem('username', data.username);
+                sessionStorage.setItem('password', data.password);
+                window.history.pushState({}, '', '/register/confirm');//todo:router
+            })
+            .catch(error => {
+                console.error('register failed:', error);
+            });
     }
 }
