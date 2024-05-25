4f2dfb791a0085c1d52d5926090f8313dfc92e6b
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-05-25 21:35:46 +0900

    :dizzy:[update]signIn form validate, error表示など

--- frontend/static_vol/js/components/UserRegister.js
+++ frontend/static_vol/js/components/UserRegister.js
@@ -2,6 +2,7 @@
 
 import PageBase from './PageBase.js';
 import { router } from '../modules/router.js';
+import { checkInputValid } from "../modules/form.js";
 
 export default class extends PageBase {
     constructor(params) {
@@ -9,91 +10,88 @@ export default class extends PageBase {
         this.setTitle('UserRegister');
         this.labelButton = '確認する'; // TODO json
         //afterRenderにmethod追加
-        this.addAfterRenderHandler(this.watchForms.bind(this));
+        //this.addAfterRenderHandler(this.watchInputs.bind(this));
         this.addAfterRenderHandler(this.listenConfirm.bind(this));
     }
 
     async renderHtml() {
         return `
-            <form class="formUserRegister blockForm" action="" method="post">
+            <form id="formUserRegister" class="formUserRegister blockForm" action="" method="post">
                 <dl class="unitFormInput">
                     <dt class="unitFormInput_label"><label for="registUsername">username</label></dt>
-                    <dd class="unitFormInput_input"><input type="text" id="registUsername" placeholder="Enter username" pattern="^(?!.*?__)[a-zA-Z0-9_]+$" minlength="3" maxlength="30" required /></dd>
+                    <dd class="unitFormInput_input">
+                        <input type="text" id="registUsername" placeholder="Enter username" pattern="^(?!.*?__)[a-zA-Z0-9_]+$" minlength="3" maxlength="30" required />
+                        <ul class="listError"></ul>
+                    </dd>
                 </dl>
                 <dl class="unitFormInput">
                     <dt class="unitFormInput_label"><label for="registPassword">password</label></dt>
-                    <dd class="unitFormInput_input"><input type="password" id="registPassword" placeholder="Enter password"
-                        pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};:'\\\\|,.<>/?~])[a-zA-Z\\d!@#$%^&*()_+\\-=\\[\\]{};:'\\\\|,.<>/?~]+$" minlength="8" required /></dd>
+                    <dd class="unitFormInput_input">
+                        <input type="password" id="registPassword" placeholder="Enter password"
+                            pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()\\\\_+\\\\-=\\\\[\\\\]{};:'\\\\\\\\|,.<>/?~]+)[a-zA-Z\\d!@#$%^&*()\\\\_+\\\\-=\\\\[\\\\]{};:'\\\\\\\\|,.<>/?~]+$" minlength="8" required />
+                        <ul class="listError"></ul>
+                    </dd>
                 </dl>
                 <dl class="unitFormInput">
                     <dt class="unitFormInput_label"><label for="registPasswordConfirm">password(confirm)</label></dt>
-                    <dd class="unitFormInput_input"><input type="password" id="registPasswordConfirm" placeholder="Enter password(confirm)"
-                        pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};:'\\\\|,.<>/?~])[a-zA-Z\\d!@#$%^&*()_+\\-=\\[\\]{};:'\\\\|,.<>/?~]+$" minlength="8" required /></dd>
+                    <dd class="unitFormInput_input">
+                        <input type="password" id="registPasswordConfirm" placeholder="Enter password(confirm)"
+                            pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()\\\\_+\\\\-=\\\\[\\\\]{};:'\\\\\\\\|,.<>/?~]+)[a-zA-Z\\d!@#$%^&*()\\\\_+\\\\-=\\\\[\\\\]{};:'\\\\\\\\|,.<>/?~]+$" minlength="8" required />
+                        <ul class="listError"></ul>
+                    </dd>
                 </dl>
                 <button type="submit" id="btnConfirmForm" class="formUserRegister_button unitButton" disabled>${this.labelButton}</button>
             </form>
         `;
     }
 
-    watchValidateForm(inputField) {
-        const classHasInput = 'has-input';
-        const addHasInput = () => {
-            inputField.classList.add(classHasInput);
-            inputField.removeEventListener('blur', addHasInput);
-        };
-
-        if (!inputField.classList.contains(classHasInput)) {
-            inputField.addEventListener('blur', addHasInput);
-        }
-    }
-
-    watchForms() {
-        const elUsername = document.getElementById('registUsername');
-        const elPassword = document.getElementById('registPassword');
-        const elPasswordConfirm = document.getElementById('registPasswordConfirm');
-        this.watchValidateForm(elUsername);
-        this.watchValidateForm(elPassword);
-        this.watchValidateForm(elPasswordConfirm);
-    }
-    
-    addErrorMessage = (elInput, message) => {
-        const classHasInput = 'has-input';
-        const elError = document.createElement('p');
-        elError.classList.add('blockError');
-        elError.textContent = message;
-        elInput.parentNode.insertBefore(elError, elInput.nextSibling);
-        if (!elInput.classList.contains(classHasInput)) {
-            elInput.classList.add(classHasInput);
-        }
-    }
-
     listenConfirm() {
         const btnConfirm = document.getElementById('btnConfirmForm');
         btnConfirm.addEventListener('click', this.handleConfirm.bind(this));
         this.addListenEvent(btnConfirm, this.handleConfirm, 'click');
+
+        const elUsername = document.getElementById('registUsername');
+        elUsername.addEventListener('blur', this.handleInput.bind(this));
+        this.addListenEvent(elUsername, this.handleInput, 'blur');
+
+        const elPassword = document.getElementById('registPassword');
+        elPassword.addEventListener('blur', this.handleInput.bind(this));
+        this.addListenEvent(elPassword, this.handleInput, 'blur');
+
+        const elPasswordConfirm = document.getElementById('registPasswordConfirm');
+        elPasswordConfirm.addEventListener('blur', this.handleInput.bind(this));
+        this.addListenEvent(elPasswordConfirm, this.handleInput, 'blur');
+    }
+
+    checkFormReady() {
+        const formRegister = document.querySelector('form');
+        const btnConfirm = document.getElementById('btnConfirmForm');
+        if (formRegister.checkValidity()) {
+            if (btnConfirm.hasAttribute('disabled')) {
+                btnConfirm.removeAttribute('disabled');
+            }
+        } else if (!btnConfirm.hasAttribute('disabled')) {
+            btnConfirm.setAttribute('disabled', '');
+        }
+    }
+
+    handleInput(ev) {
+        const elInput = ev.target;
+        //初回入力時、invalid styleが当たるようにclass付与
+        const classHasInput = 'has-input';
+        if (!elInput.classList.contains(classHasInput)) {
+            elInput.classList.add(classHasInput);
+        }
+        //formの各input validate
+        checkInputValid(elInput);
+        //ボタンenabled切り替え(ok=>ngもありうる)
+        this.checkFormReady();
     }
 
     handleConfirm(ev) {
         ev.preventDefault();
         const elUsername = document.getElementById('registUsername');
         const elPassword = document.getElementById('registPassword');
-        const elPasswordConfirm = document.getElementById('registPasswordConfirm');
-        let numError = 0;
-        if (!elUsername.validity.patternMismatch) {
-            this.addErrorMessage(elUsername, 'username is invalid.');
-            numError++;
-        }
-        if (!elPassword.validity.patternMismatch) {
-            this.addErrorMessage(elPassword, 'password is invalid.');
-            numError++;
-        }
-        if (!elPassword.value !== elPasswordConfirm.value) {
-            this.addErrorMessage(elPasswordConfirm, 'The passwords you entered are not the same.');
-            numError++;
-        }
-        if (numError !== 0) {
-            return ;
-        }
 
         const data = {
             username: elUsername.value,
@@ -114,6 +112,7 @@ export default class extends PageBase {
                 return response.json();
             })
             .then( async () => {
+                //確認画面用にsessionStorageに保存。送信時削除
                 sessionStorage.setItem('username', data.username);
                 sessionStorage.setItem('password', data.password);
                 history.pushState(null, null, '/register/confirm');
---
+++ frontend/static_vol/js/modules/form.js
@@ -0,0 +1,74 @@
+'use strict';
+
+import PageList from "../components/PageList.js";
+import Home from "../components/Home.js";
+import UserRegister from "../components/UserRegister.js";
+import UserRegisterConfirm from "../components/UserRegisterConfirm.js";
+import UserRegisterComplete from "../components/UserRegisterComplete.js";
+import User from "../components/User.js";
+import GamePlay from "../components/GamePlay.js";
+import GameMatch from "../components/GameMatch.js";
+import TournamentEntry from "../components/TournamentEntry.js";
+import TournamentMatch from "../components/TournamentMatch.js";
+
+const errorMessages = {
+    'valueMissing': 'This field is required.',
+    'patternMismatch': 'The character types used do not meet the requirement.',
+    'tooShortOrTooLong': 'The character count does not meet the requirement.'
+};
+
+const addErrorMessage = (errWrapper, errorType) => {
+    const elError = document.createElement('li');
+    elError.textContent = errorMessages[errorType];
+    elError.setAttribute('data-error-type', errorType);
+    errWrapper.appendChild(elError);
+}
+
+const checkInputValid = (elInput) => {
+    const errWrapper = elInput.parentNode.querySelector('.listError');
+    const liValueMissing = errWrapper.querySelector('li[data-error-type="valueMissing"]');
+    const liPatternMismatch = errWrapper.querySelector('li[data-error-type="patternMismatch"]');
+    const liTooShortOrTooLong = errWrapper.querySelector('li[data-error-type="tooShortOrTooLong"]');
+    const validityState = elInput.validity;
+    if (validityState.valid) {
+        if (liValueMissing) {
+            liValueMissing.remove();
+        }
+        if (liPatternMismatch) {
+            liPatternMismatch.remove();
+        }
+        if (liTooShortOrTooLong) {
+            liTooShortOrTooLong.remove();
+        }
+        return true;
+    }
+    //これ以降はinvalid。error表示してreturn false
+    // requiredが未入力
+    if (validityState.valueMissing) {
+        if (!liValueMissing) {
+            addErrorMessage(errWrapper, 'valueMissing');
+        }
+    } else if (liValueMissing) {
+        liValueMissing.remove();
+    }
+    // patternを満たさない
+    if (validityState.patternMismatch) {
+        if (!liPatternMismatch) {
+            addErrorMessage(errWrapper, 'patternMismatch');
+        }
+    } else if (liPatternMismatch) {
+        liPatternMismatch.remove();
+    }
+    // 文字数が規定に反する
+    if (validityState.tooShort || validityState.tooLong) {
+        if (!liTooShortOrTooLong) {
+            addErrorMessage(errWrapper, 'tooShortOrTooLong');
+        }
+    } else if (liTooShortOrTooLong) {
+        liTooShortOrTooLong.remove();
+    }
+    // todo: min, max, step, typeは設定箇所が無いためチェックなし
+    return false;
+};
+
+export { checkInputValid };
