48dbab4dadaf0d504dd5a42af30aef00ecdb329a
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-06-08 20:42:23 +0900

    :sparkles:[add]dashboard: add" Received friend request block"

--- frontend/static_vol/js/components/Dashboard.js
+++ frontend/static_vol/js/components/Dashboard.js
@@ -9,9 +9,11 @@ import { join_game } from '../modules/match.js';
 export default class extends PageBase {
     constructor(params) {
         super(params);
+        this.setTitle(`Dashboard`);
         this.labelMatch = '対戦する';
         this.labelCancel = 'キャンセル';
-        this.setTitle(`USER: ${this.userName}`);
+        this.labelAccept = '承諾';
+        this.labelReject = '拒絶';
         //afterRenderにmethod追加
         this.addAfterRenderHandler(this.showUserList.bind(this));
     }
@@ -22,12 +24,58 @@ export default class extends PageBase {
                 <div class="blockPlayerDetail_profile">
                     <p class="blockPlayerDetail_thumb thumb"><img src="//ui-avatars.com/api/?name=Gg Hh&background=872bac&color=ffffff" alt="" width="200" height="200"></p>
                     <p class="blockPlayerDetail_score unitBox">RANK: 4 <br>(70勝20敗)</p>
+                    <ul class="unitListBtn unitListBtn-w100">
+                        <li><a href="/lounge" class="unitButton" data-link>Lounge</a></li>
+                        <li><a href="/tournament" class="unitButton" data-link>Tournament</a></li>
+                    </ul>
                 </div>
                 <div class="blockPlayerDetail_detail">
+                    <section class="blockFriendRequest">
+                        <h3 class="blockFriendRequest_title unitTitle2">Received friend request</h3>
+                        <div class="blockFriendRequest_friends listFriends listLineDivide">
+                            <section class="unitFriend">
+                                <header class="unitFriend_header">
+                                    <h4 class="unitFriend_name">username</h4>
+                                    <p class="unitFriend_thumb"><img src="//ui-avatars.com/api/?name=username&background=3cbbc9&color=ffffff" alt="" width="100" height="100"></p>
+                                </header>
+                                <p class="unitFriendButton unitFriend_button-match">
+                                    <button type="button" class="unitFriendButton_matchRequest unitButton" data-name="username" data-avatar="//ui-avatars.com/api/?name=username&background=3cbbc9&color=ffffff">${this.labelAccept}</button>
+                                    <button type="button" class="unitFriendButton_matchRequest unitButton" data-name="username" data-avatar="//ui-avatars.com/api/?name=username&background=3cbbc9&color=ffffff">${this.labelReject}</button>
+                                </p>
+                            </section>
+                            <section class="unitFriend">
+                                <header class="unitFriend_header">
+                                    <h4 class="unitFriend_name">username</h4>
+                                    <p class="unitFriend_thumb"><img src="//ui-avatars.com/api/?name=username&background=3cbbc9&color=ffffff" alt="" width="100" height="100"></p>
+                                </header>
+                                <p class="unitFriendButton unitFriend_button-match">
+                                    <button type="button" class="unitFriendButton_matchRequest unitButton" data-name="username" data-avatar="//ui-avatars.com/api/?name=username&background=3cbbc9&color=ffffff">${this.labelAccept}</button>
+                                    <button type="button" class="unitFriendButton_matchRequest unitButton" data-name="username" data-avatar="//ui-avatars.com/api/?name=username&background=3cbbc9&color=ffffff">${this.labelReject}</button>
+                                </p>
+                            </section>
+                            <section class="unitFriend">
+                                <header class="unitFriend_header">
+                                    <h4 class="unitFriend_name">username</h4>
+                                    <p class="unitFriend_thumb"><img src="//ui-avatars.com/api/?name=username&background=3cbbc9&color=ffffff" alt="" width="100" height="100"></p>
+                                </header>
+                                <p class="unitFriendButton unitFriend_button-match">
+                                    <button type="button" class="unitFriendButton_matchRequest unitButton" data-name="username" data-avatar="//ui-avatars.com/api/?name=username&background=3cbbc9&color=ffffff">${this.labelAccept}</button>
+                                    <button type="button" class="unitFriendButton_matchRequest unitButton" data-name="username" data-avatar="//ui-avatars.com/api/?name=username&background=3cbbc9&color=ffffff">${this.labelReject}</button>
+                                </p>
+                            </section>
+                        </div>
+                    </section>
                     <section class="blockFriends">
                         <h3 class="blockFriends_title unitTitle1">Friends</h3>
-                        <div class="blockFriends_friends listFriends listLineDivide">
-                        </div>
+                        <div class="blockFriends_friends listFriends listLineDivide"></div>
+                    </section>
+                    <section class="blockMatchLog">
+                        <h3 class="blockMatchLog_title unitTitle1">Tournament Log</h3>
+                        <ul class="blockMatchLog_log listMatchLog listLineDivide">
+                            <li class="listMatchLog_item"><strong>RANK: 4</strong> <span>(2024/4/2 tournament52)</span></li>
+                            <li class="listMatchLog_item"><strong>RANK: 6</strong> <span>(2024/4/1 tournament45)</span></li>
+                            <li class="listMatchLog_item"><strong>RANK: 4</strong> <span>(2024/3/24 tournament42)</span></li>
+                        </ul>
                     </section>
                     <section class="blockMatchLog">
                         <h3 class="blockMatchLog_title unitTitle1">Match Log</h3>
@@ -35,7 +83,6 @@ export default class extends PageBase {
                             <li class="listMatchLog_item"><strong>RANK: 4</strong> <span>(2024/4/2 tournament52)</span></li>
                             <li class="listMatchLog_item"><strong>RANK: 6</strong> <span>(2024/4/1 tournament45)</span></li>
                             <li class="listMatchLog_item"><strong>RANK: 4</strong> <span>(2024/3/24 tournament42)</span></li>
-                            <li class="listMatchLog_item"><strong>RANK: 1</strong> <span>(2024/3/10 tournament40)</span></li>
                         </ul>
                     </section>
                 </div>
@@ -44,7 +91,7 @@ export default class extends PageBase {
     }
 
     showUserList() {
-        const listFriendsWrapper = document.querySelector('.listFriends');
+        const listFriendsWrapper = document.querySelector('.blockFriends_friends');
         getUserList()
             .then(data => {
                 listFriendsWrapper.innerHTML = '';
@@ -55,7 +102,7 @@ export default class extends PageBase {
                             <p class="unitFriend_thumb"><img src="//ui-avatars.com/api/?name=${user.username}&background=3cbbc9&color=ffffff" alt="" width="100" height="100"></p>
                         </header>
                         <p class="unitFriendButton unitFriend_button-match">
-                            <button type="submit" class="unitFriendButton_matchRequest unitButton" data-name="${user.username}" data-avatar="//ui-avatars.com/api/?name=${user.username}&background=3cbbc9&color=ffffff">${this.labelMatch}</button>
+                            <button type="button" class="unitFriendButton_matchRequest unitButton" data-name="${user.username}" data-avatar="//ui-avatars.com/api/?name=${user.username}&background=3cbbc9&color=ffffff">${this.labelMatch}</button>
                         </p>
                     </section>
                   `);
