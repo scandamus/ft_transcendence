a07f10bb496fecba9ab001baea3d9b7e17c44b83
Monksoffunk <monksoffunk@users.noreply.github.com>
2024-06-16 18:39:40 +0900

    Move join_game to a separate file

---
+++ backend/scandamus/game/join_game.py
@@ -0,0 +1,110 @@
+import json
+import jwt
+import logging
+
+# from channels.generic.websocket import AsyncWebsocketConsumer
+from django.contrib.auth.models import User
+from .models import Player
+from .models import Match
+from channels.db import database_sync_to_async
+from datetime import datetime, timedelta
+from rest_framework_simplejwt.backends import TokenBackend
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError, TokenBackendError
+from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
+from .utils import generate_game_jwt
+from django.conf import settings
+from django.db import transaction
+
+logger = logging.getLogger(__name__)
+
+async def handle_join_game(consumer, token):
+    user, error = await authenticate_token(token)
+    if user:
+        player = await get_player_from_user(user)
+        consumer.players[user.username] = {
+            'user': user,
+            'players_id': player.id,
+            'websocket': consumer
+        }
+        logger.info(f"user={user.username} requesting new game match")
+        if len(consumer.players) == 2:
+            players_list = list(consumer.players.values())
+            player1 = await get_player_from_user(players_list[0]['user'])
+            player2 = await get_player_from_user(players_list[1]['user'])
+            match = await create_match(player1, player2)
+            for player in players_list:
+                game_token = await issue_jwt(player['user'], player['players_id'], match.id)
+                await player['websocket'].send(text_data=json.dumps({
+                    'type': 'gameSession',
+                    'jwt': game_token,
+                    'username': player['user'].username,
+                    'match_id': match.id
+                }))
+                consumer.players.clear()
+    else:
+        logger.error('Error: Authentication Failed')
+        logger.error(f'error={error}')
+        await consumer.send(text_data=json.dumps({
+            'type': 'authenticationFailed',
+            'message': error
+        }))
+        
+async def handle_join_game_cancel(consumer):
+    if hasattr(consumer, 'user') and consumer.user.username in consumer.players:
+        del consumer.players[consumer.user.username]
+        logger.info(f'{consumer.user.username}: cancel accepted.')
+
+@database_sync_to_async
+def get_player_from_user(user):
+    try:
+        return Player.objects.get(user=user)
+    except Player.DoesNotExist:
+        logger.info(f"Player doesn't exist for user: {user.username}")
+        return None
+
+@database_sync_to_async
+def issue_jwt(user, players_id, match_id):
+    expire = datetime.utcnow() + timedelta(minutes=1)
+    payload = {
+        'user_id': user.id,
+        'username': user.username,
+        'players_id': players_id,
+        'match_id': match_id,
+        'iat': datetime.utcnow(),
+        'exp': expire,
+        'aud': 'pong-server',
+        'iss': 'backend'
+    }
+    token = jwt.encode(payload, settings.SIMPLE_JWT['SIGNING_KEY'], algorithm='HS256')
+    return token
+
+@database_sync_to_async
+def create_match(player1, player2):
+    # マッチをDBに登録
+    with transaction.atomic():
+        match = Match.objects.create(
+            player1=player1,
+            player2=player2,
+            status='before'
+        )
+    logger.info(f'Match created, {match}, ID: {match.id}, Player1: {match.player1}, Player2: {match.player2}')
+    return match
+
+@database_sync_to_async
+def authenticate_token(token):
+    try:
+        token_backend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'], signing_key=settings.SIMPLE_JWT['SIGNING_KEY'])
+        validated_token = token_backend.decode(token, verify=True)
+        user_id = validated_token['user_id']
+        user = User.objects.get(id=user_id)
+        #self.user = user
+        return user, None
+    except TokenBackendError as e:
+        logger.info(f"TokenBackendError: " + str(e))
+        return None, 'Error: Token Backend Error.'
+    except (InvalidToken, TokenError) as e:
+        logger.info(f"Error decoding token: {str(e)}")
+        return None, 'Error: Token is invalid or expired.'
+    except (User.DoesNotExist) as e:
+        logger.info(f"User not found")
+        return None, 'User not found'
