90e3278c118708bcd02bc57a95bb8e50a4531adb
chanma <yunosuke.homma@icloud.com>
2024-06-24 03:24:28 +0900

    [update]patchリクエストをapiに送信して戦績を変更できるように(jwtの認証なし)

--- pong-server/game-server/pong/api_access.py
+++ pong-server/game-server/pong/api_access.py
@@ -19,3 +19,24 @@ def get_match_from_api(match_id):
     except requests.exceptions.RequestException as e:
         logger.error('Error: ', e)
     return None
+
+
+def patch_match_to_api(storage_token, match_id, send_data):
+    try:
+        url = f'http://backend:8001/api-internal/game/match/{match_id}/'
+        logger.info(f'patch_match_to_api: start')
+        headers = {
+            'Authorization': f'Bearer {storage_token}',
+            'content_type': 'application/json',
+        }
+        response = requests.patch(url, data=send_data, headers=headers)
+        response.raise_for_status()
+        logger.info(f'API response: {response.json()}')
+    except requests.exceptions.HTTPError as e:
+        logger.error('HTTP Errpr: ', e)
+    except requests.exceptions.ConnectionError as e:
+        logger.error('Error Connecting: ', e)
+    except requests.exceptions.Timeout as e:
+        logger.error('Error Timeout: ', e)
+    except requests.exceptions.RequestException as e:
+        logger.error('Error: ', e)
