b6af826a1924f478560614a56d31463a4f11ce1d
chanma <yunosuke.homma@icloud.com>
2024-06-24 14:18:53 +0900

    [fix]logger.errorの出力形式

--- pong-server/game-server/pong/api_access.py
+++ pong-server/game-server/pong/api_access.py
@@ -3,6 +3,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 def get_match_from_api(match_id):
     try:
         logger.info(f'get_match_from_api: start')
@@ -11,13 +12,13 @@ def get_match_from_api(match_id):
         logger.info(f'API response: {response.json()}')
         return response.json()
     except requests.exceptions.HTTPError as e:
-        logger.error('HTTP Error: ', e)
+        logger.error('HTTP Error: %s', e)
     except requests.exceptions.ConnectionError as e:
-        logger.error('Error Connecting: ', e)
+        logger.error('Error Connecting: %s', e)
     except requests.exceptions.Timeout as e:
-        logger.error('Error Timeout: ', e)
+        logger.error('Error Timeout: %s', e)
     except requests.exceptions.RequestException as e:
-        logger.error('Error: ', e)
+        logger.error('Error: %s', e)
     return None
 
 
@@ -33,10 +34,10 @@ def patch_match_to_api(storage_token, match_id, send_data):
         response.raise_for_status()
         logger.info(f'API response: {response.json()}')
     except requests.exceptions.HTTPError as e:
-        logger.error('HTTP Error: ', e)
+        logger.error('HTTP Error: %s', e)
     except requests.exceptions.ConnectionError as e:
-        logger.error('Error Connecting: ', e)
+        logger.error('Error Connecting: %s', e)
     except requests.exceptions.Timeout as e:
-        logger.error('Error Timeout: ', e)
+        logger.error('Error Timeout: %s', e)
     except requests.exceptions.RequestException as e:
-        logger.error('Error: ', e)
+        logger.error('Error: %s', e)
