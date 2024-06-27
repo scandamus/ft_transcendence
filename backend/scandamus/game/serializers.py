03960de2c2062f81304f641e36af11ac93df0974
Monksoffunk <monksoffunk@users.noreply.github.com>
2024-06-28 08:45:12 +0900

    Add update method to Match class
    Change Player status waiting if Match status set to after

--- backend/scandamus/game/serializers.py
+++ backend/scandamus/game/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Tournament, Match, Entry
+from players.models import Player
 
 
 class TournamentSerializer(serializers.ModelSerializer):
@@ -12,7 +13,21 @@ class MatchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Match
         fields = '__all__'
+    
+    def update(self, instance, validated_data):
+        old_status = instance.status
+        new_status = validated_data.get('status', instance.status)
 
+        if old_status != 'after' and new_status == 'after':
+            self.set_players_to_waiting(instance)
+
+    def set_players_to_waiting(self, match):
+        players = [match.player1, match.player2, match.player3, match.player4]
+        for player in players:
+            if player:
+                player.status = 'waiting'
+                player.current_match = None
+                player.save()
 
 class EntrySerializer(serializers.ModelSerializer):
     class Meta:
