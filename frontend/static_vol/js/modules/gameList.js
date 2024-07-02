2beec56141bfa9ebe3f5b21c67d2adc30c11fa9f
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-07-03 03:24:25 +0900

    :dizzy:[update]MatchLog: json取得=> 描画

--- frontend/static_vol/js/modules/gameList.js
+++ frontend/static_vol/js/modules/gameList.js
@@ -2,6 +2,7 @@
 
 import { fetchMatchLog } from "./gameApi.js";
 import { labels } from "./labels.js";
+import { SiteInfo } from "./SiteInfo.js";
 
 const getMatchLog = async () => {
     console.log('getMatchLog');
@@ -11,26 +12,46 @@ const getMatchLog = async () => {
         if (!logList || logList.length === 0) {
             console.log('no match log')
         } else {
-            listRequestWrapper.innerHTML = '';
+            const siteInfo = new SiteInfo();
+            const myUsername = siteInfo.getUsername();
+            const myAvatar = siteInfo.getAvatar();
+            // listRequestWrapper.innerHTML = '';
             logList.forEach(logItem => {
+                const myResultHTML = `
+                    <section class="blockMatch_player unitMatchPlayer">
+                        <header class="unitMatchPlayer_header">
+                            <img src="${myAvatar}" alt="" width="50" height="50">
+                            <h4 class="unitMatchPlayer_title">${myUsername}</h4>
+                        </header>
+                        <p class="unitMatchPlayer_score">${logItem.my_score}</p>
+                        ${logItem.is_win ? '<p class="unitMatchPlayer_result">win</p>' : ''}
+                    </section>
+                `;
+
+                // 対戦相手のスコア部分を作成
+                let opponentsResultHTML = '';
+                logItem.players.forEach(player => {
+                    const avatar = player.avatar ? player.avatar : '/images/avatar_default.png';
+                    opponentsResultHTML += `
+                        <section class="blockMatch_player unitMatchPlayer">
+                            <header class="unitMatchPlayer_header">
+                                <img src="${avatar}" alt="" width="50" height="50">
+                                <h4 class="unitMatchPlayer_title">${player.username}</h4>
+                            </header>
+                            <p class="unitMatchPlayer_score">${player.score}</p>
+                            ${player.is_win ? '<p class="unitMatchPlayer_result">win</p>' : ''}
+                        </section>
+                    `;
+                });
+
+                // 全体のHTMLを結合
                 const requestElement = `
                     <div class="blockMatch">
-                        <section class="blockMatch_player unitMatchPlayer">
-                            <header class="unitMatchPlayer_header">
-                                <img src="//ui-avatars.com/api/?name=Aa Bb&background=e3ad03&color=ffffff" alt="" width="50" height="50">
-                                <h4 class="unitMatchPlayer_title">01234567890123456789012345678901</h4>
-                            </header>
-                            <p class="unitMatchPlayer_score">5</p>
-                        </section>
+                        ${myResultHTML}
                         <p class="blockMatch_vs">VS</p>
-                        <section class="blockMatch_player unitMatchPlayer">
-                            <header class="unitMatchPlayer_header">
-                                <img src="//ui-avatars.com/api/?name=aa&background=3cbbc9&color=ffffff" alt="" width="50" height="50">
-                                <h4 class="unitMatchPlayer_title">username</h4>
-                            </header>
-                            <p class="unitMatchPlayer_score">10</p>
-                            <p class="unitMatchPlayer_result">win</p>
-                        </section>
+                        <div class="blockMatch_opponents">
+                            ${opponentsResultHTML}
+                        </div>
                     </div>
                 `;
                 listRequestWrapper.innerHTML += requestElement;
