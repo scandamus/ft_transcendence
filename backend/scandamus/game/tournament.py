4f5ddf9d2c02e45c5ef58cca168626184fc171bf
Monksoffunk <monksoffunk@users.noreply.github.com>
2024-07-03 10:24:31 +0900

    Handle entry tournament request (backend)

--- backend/scandamus/game/tournament.py
+++ backend/scandamus/game/tournament.py
@@ -4,7 +4,7 @@ import logging
 
 from django.contrib.auth.models import User
 from .models import Player
-from .models import Tournament
+from .models import Tournament, Entry
 from django.conf import settings
 from django.db import transaction, IntegrityError
 from channels.db import database_sync_to_async
@@ -82,3 +82,138 @@ def create_tournament(data):
     except Exception as e:
         logger.error(f'Error in tournament json: {e}')
         return None, None
+
+async def handle_entry_tournament(consumer, data):
+    user, error = await authenticate_token(data['token'])
+    if not user:
+        logger.error('Error: Authentication Failed')
+        logger.error(f'error={error}')
+        await consumer.send(text_data=json.dumps({
+            'type': 'authenticationFailed',
+            'message': error
+        }))
+        return
+
+    player = await get_player_by_user(user)
+    if not player:
+        logger.error(f"No player found for user: {user.username}")
+        await send_entry_error(consumer, 'invalidPlayer')
+        return
+    
+    tournament, nickname = await get_tournament_and_nickname(consumer, data)
+    if tournament is None:
+        await send_entry_error(consumer, 'invalidTournament')
+        return
+
+    entry = await get_entry(tournament, player)
+    if entry:
+        logger.info(f"Entry already exists for player {user.username} in tournament {tournament.name}")
+        await send_entry_error(consumer, 'alreadyEnterd')
+        return
+
+    if await is_duplicate_nickname(tournament, nickname):
+        logger.info(f'Nickname {nickname} already exists in tournament {tournament}')
+        await send_entry_error(consumer, 'duplicateNickname')
+        return
+
+    result = await create_entry(tournament, player, nickname)
+    if result is 'capacityFull':
+        logger.info(f'{tournament.name} capacity is full')
+        await send_entry_error(consumer, 'capacityFull')
+        return
+    
+    logger.info(f'nickname {result.nickname} just entried {tournament.name}')
+    await consumer.send(text_data=json.dumps({
+        'type': 'tournament',
+        'action': 'entryDone',
+        'name': tournament.name
+    }))
+
+async def handle_cancel_entry(consumer, data):
+    user, error = await authenticate_token(data['token'])
+    if not user:
+        logger.error('Error: Authentication Failed')
+        logger.error(f'error={error}')
+        await consumer.send(text_data=json.dumps({
+            'type': 'authenticationFailed',
+            'message': error
+        }))
+        return
+
+    player = await get_player_by_user(user)
+    if not player:
+        logger.error(f"No player found for user: {user.username}")
+        await send_entry_error(consumer, 'invalidPlayer')
+        return
+    
+    tournament, nickname = await get_tournament_and_nickname(consumer, data)
+    if tournament is None:
+        await send_entry_error(consumer, 'invalidTournament')
+        return
+
+    entry = await get_entry(tournament, player)
+    if entry is None:
+        logger.info(f'Entry doesnot exist for player {user.username} in tournament {tournament.name}')
+        await send_entry_error(consumer, 'invalidCancelRequest')
+        return
+
+    await remove_entry(tournament, player)
+
+    logger.info(f'Entry for player {user.username} in tournament {tournament.name} has been removed')
+    await consumer.send(text_data=json.dumps({
+        'type': 'tournament',
+        'action': 'removeEntryDone',
+        'name': tournament.name
+    }))
+
+@database_sync_to_async
+def get_tournament_and_nickname(consumer, data):
+    try:
+        id = data.get('id', None)
+        name = data.get('name', None)
+        nickname = data.get('nickname', None)
+        
+        if id is None or name is None or nickname is None:
+            logger.error(f'Error entry_tournament socket json id:{id}, name:{name}, nickname:{nickname}')
+            return None, None
+        
+        tournament = Tournament.objects.get(id=id)
+        if tournament or tournament.status is not 'upcoming': # 本来upcomingではないリクエストは来ない
+            logger.info(f'{tournament} found in database')
+            return tournament, nickname
+        logger.error(f'{name} is not found')
+        return None, None
+
+    except Exception as e:
+        logger.error(f'Error in get_tournament: {e}')
+        return None, None
+
+@database_sync_to_async
+def get_entry(tournament, player):
+    try:
+        return Entry.objects.get(tournament=tournament, player=player)
+    except Entry.DoesNotExist:
+        return None
+
+@database_sync_to_async
+def is_duplicate_nickname(tournament, nickname):
+    return Entry.objects.filter(tournament=tournament, nickname=nickname).exists()
+
+@database_sync_to_async
+def create_entry(tournament, player, nickname):
+    with transaction.atomic():
+        if Entry.objects.filter(tournament=tournament).count() >= tournament.max_participants:
+            return 'capacityFull'
+        return Entry.objects.create(tournament=tournament, player=player, nickname=nickname)
+
+@database_sync_to_async
+def remove_entry(tournament, player):
+    Entry.objects.filter(tournament=tournament, player=player).delete()
+
+
+async def send_entry_error(websocket, action):
+    await websocket.send(text_data=json.dumps({
+        'type': 'tournament',
+        'action': action
+    }))
+    logger.error(f'sent error to user: {action}')
