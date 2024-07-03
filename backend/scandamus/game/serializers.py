f6d595cd0feb12a938a2f89220e7d1d2565c8620
Monksoffunk <monksoffunk@users.noreply.github.com>
2024-07-03 10:23:22 +0900

    Update Tournament class

--- backend/scandamus/game/serializers.py
+++ backend/scandamus/game/serializers.py
@@ -3,11 +3,25 @@ from .models import Tournament, Match, Entry
 
 
 class TournamentSerializer(serializers.ModelSerializer):
+    current_participants = serializers.SerializerMethodField()
+    nickname = serializers.SerializerMethodField()
     class Meta:
         model = Tournament
         fields = '__all__'
 
-
+    def get_current_participants(self, obj):
+        return Entry.objects.filter(tournament=obj).count()
+    
+    def get_nickname(self, obj):
+        request = self.context.get('request', None)
+        if request and request.user.is_authenticated:
+            player = request.user.player
+            try:
+                entry = Entry.objects.get(tournament=obj, player=player)
+                return entry.nickname
+            except Entry.DoesNotExist:
+                return ''
+        return ''
 class MatchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Match
