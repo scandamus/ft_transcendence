8281b9b942bd1557e56ca1c7d3ef44d717a3f496
Monksoffunk <monksoffunk@users.noreply.github.com>
2024-07-14 23:39:49 +0900

    Remove caller duplication

--- backend/scandamus/players/auth.py
+++ backend/scandamus/players/auth.py
@@ -84,7 +84,6 @@ async def handle_auth(consumer, token):
                     'type': 'ack',
                     'message': 'Authentication successful'
                 }))
-                await send_status_to_friends(player, 'online')
             except () as e:
                 logger.error(f'Error in handle_auth can not send')
 
