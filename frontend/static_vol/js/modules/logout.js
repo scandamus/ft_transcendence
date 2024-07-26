e29d864577d229aaef51385f670c99b111619399
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-07-26 22:54:54 +0900

    [update]remove tournament data on logout

--- frontend/static_vol/js/modules/logout.js
+++ frontend/static_vol/js/modules/logout.js
@@ -49,6 +49,10 @@ const handleLogout = (ev) => {
             //token rm
             sessionStorage.removeItem('accessToken');
             sessionStorage.removeItem('refreshToken');
+            sessionStorage.removeItem('all_usernames');
+            sessionStorage.removeItem('player_name');
+            sessionStorage.removeItem('tournament_id');
+            sessionStorage.removeItem('tournament_status');
             webSocketManager.closeWebSocket('lounge');
             webSocketManager.closeWebSocket('pong');
             const siteInfo = new SiteInfo();
