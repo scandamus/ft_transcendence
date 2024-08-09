36ed3982c27cb302cd541286ae4b8fc93651f3ae
chanma <yunosuke.homma@icloud.com>
2024-08-10 01:34:45 +0900

    [fix]デバッグ用の値を戻した

--- pong4-server/game-server/pong4/game_logic.py
+++ pong4-server/game-server/pong4/game_logic.py
@@ -85,7 +85,7 @@ class Ball:
 
     def reset(self, x, y):
         self.speed = 6
-        tmp = self.get_ball_direction_and_random_speed(random.randint(0, 0), random.choice((-1, 1)))
+        tmp = self.get_ball_direction_and_random_speed(random.randint(-90, 90), random.choice((-1, 1)))
         self.x = x - BALL_SIZE / 2
         self.y = y - BALL_SIZE / 2
         self.dx = tmp['dx']
